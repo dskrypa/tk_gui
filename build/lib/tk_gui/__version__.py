5509e6429928691ba32efe593f8b4fa67e6ebc0f
dskrypa <dskrypa@gmail.com>
2023-01-26 07:47:30 -0500

    fixed gitignore

--- build/lib/tk_gui/__version__.py
+++
@@ -1,8 +0,0 @@
-
-__title__ = 'tk_gui'
-__description__ = 'TK GUI'
-__url__ = 'https://github.com/dskrypa/tk_gui'
-__version__ = '2023.01.26'
-__author__ = 'Doug Skrypa'
-__author_email__ = 'dskrypa@gmail.com'
-__copyright__ = 'Copyright 2022 Doug Skrypa'
