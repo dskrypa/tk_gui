8518e19ab38e1b5d757c0c928a4772ffea3349ca
dskrypa <dskrypa@gmail.com>
2023-02-26 14:19:50 -0500

    converted ClockView to actually be a View

--- lib/tk_gui/views/clock.py
+++ lib/tk_gui/views/clock.py
@@ -8,11 +8,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from ..elements.images import ClockImage
-from ..event_handling import event_handler
-from ..popups.image import AnimatedPopup
-from ..styles import Style
-from ..window import Window
+from tk_gui.caching import cached_property
+from tk_gui.elements.images import ClockImage
+from tk_gui.event_handling import event_handler
+from tk_gui.positioning import positioner
+from tk_gui.styles import Style
+from .view import View
 
 if TYPE_CHECKING:
     from tkinter import Event
@@ -21,26 +22,38 @@ if TYPE_CHECKING:
 __all__ = ['ClockView']
 
 
-class ClockView(AnimatedPopup):
-    gui_image: ClockImage
-
+class ClockView(View):
     def __init__(
         self, img_size: XY = None, fg: Color = '#FF0000', bg: Color = '#000000', seconds: bool = True, **kwargs
     ):
         self._img_size = img_size
+        self._last_size = img_size
         self._toggle_slim_on_click = '<Button-2>'
         self._clock_kwargs = {'fg': fg, 'bg': bg, 'seconds': seconds}
+        self.window_kwargs = {
+            'config_name': self.__class__.__name__,
+            'exit_on_esc': True,
+            'grab_anywhere': True,
+            'no_title_bar': True,
+            'margins': (0, 0),
+            'style': Style(bg=bg),
+            'alpha_channel': 0.8,
+        }
         kwargs.setdefault('title', 'Clock')
-        kwargs.setdefault('config_name', self.__class__.__name__)
-        kwargs.setdefault('margins', (0, 0))
-        kwargs.setdefault('style', Style(bg=bg))
-        kwargs.setdefault('alpha_channel', 0.8)
-        kwargs.setdefault('grab_anywhere', True)
-        kwargs.setdefault('no_title_bar', True)
-        super().__init__(None, **kwargs)
+        super().__init__(**kwargs)
 
-    def prepare_window(self) -> Window:
-        return Window(self.get_layout(), title=self.title, is_popup=False, **self.window_kwargs)
+    def get_pre_window_layout(self):
+        return [[self.gui_image]]
+
+    @cached_property
+    def gui_image(self) -> ClockImage:
+        kwargs = {'toggle_slim_on_click': self._toggle_slim_on_click, 'pad': (2, 2), **self._clock_kwargs}
+        if img_size := self._img_size:
+            if (parent := self.parent) and (monitor := positioner.get_monitor(*parent.position)):
+                width, height = img_size
+                img_size = min(monitor.width - 70, width or 0), min(monitor.height - 70, height or 0)
+            kwargs['img_size'] = img_size
+        return ClockImage(**kwargs)
 
     @event_handler('<Button-3>')
     def toggle_title(self, event: Event):
@@ -57,16 +70,22 @@ class ClockView(AnimatedPopup):
         width, height = image.clock.time_size()
         image.resize(width, height - 10)
 
-    def _set_image(self, image: None):
-        self._empty = False
-        kwargs = {'toggle_slim_on_click': self._toggle_slim_on_click, 'pad': (2, 2), **self._clock_kwargs}
-        if img_size := self._img_size:
-            self.orig_size = img_size
-            self._last_size = init_size = self._init_size()
-            self.gui_image = ClockImage(img_size=init_size, **kwargs)
-        else:
-            self.gui_image = ClockImage(**kwargs)
-            self.orig_size = self._last_size = self.gui_image.size
+    @event_handler('SIZE_CHANGED')
+    def handle_size_changed(self, event: Event, size: XY):
+        if self._last_size == size or not (new_size := self._get_new_size(*size)):
+            return
+        self._last_size = size
+        self.gui_image.resize(*new_size)
+        self.window.set_title(self.title)
+
+    def _get_new_size(self, new_w: int, new_h: int) -> XY | None:
+        image = self.gui_image
+        px, py = image.pad
+        new_size = (new_w - px * 2 - 2, new_h - py * 2 - 2)
+        new_img_size = image.target_size(*new_size)
+        if new_img_size != image.size:
+            return new_size
+        return None
 
 
 if __name__ == '__main__':
