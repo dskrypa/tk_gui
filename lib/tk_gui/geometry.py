8f8e760f47276f2eaa812fe9ebe0768c1e07184b
dskrypa <dskrypa@gmail.com>
2023-03-18 15:43:29 -0400

    added additional geometry methods to resize a box to fit in another

--- lib/tk_gui/geometry.py
+++ lib/tk_gui/geometry.py
@@ -30,7 +30,7 @@ class Sized(ABC):
     def height(self) -> int:
         raise NotImplementedError
 
-    @cached_property
+    @cached_property(block=False)
     def size(self) -> XY:
         return self.width, self.height
 
@@ -40,7 +40,7 @@ class Sized(ABC):
 
     # region Aspect Ratio
 
-    @cached_property
+    @cached_property(block=False)
     def aspect_ratio(self) -> float:
         width, height = self.size
         return width / height
@@ -92,6 +92,18 @@ class Sized(ABC):
             elif dst_h is None:
                 return floor(dst_w), src_h
 
+    def fit_inside_size(self, size: XY, keep_ratio: bool = True) -> XY:
+        """Determine a target size that would make this object's size fit inside a box of the given size"""
+        out_w, out_h = size
+        src_w, src_h = self.size
+        w_ok, h_ok = src_w <= out_w, src_h <= out_h
+        if w_ok and h_ok:
+            return src_w, src_h
+        elif w_ok or h_ok:
+            return self.target_size((src_w, out_h) if w_ok else (out_w, src_h), keep_ratio)
+        else:
+            return self.target_size(size, keep_ratio)
+
     def scale_size(self, size: OptXYF, keep_ratio: bool = True) -> XY:
         """
         Scale this object's size to as close to the given target size as possible, optionally respecting aspect ratio.
@@ -138,21 +150,51 @@ class Box(Sized):
     def __contains__(self, other: Box) -> bool:
         return self.contains_x(other) and self.contains_y(other)
 
-    def contains_x(self, other: Box) -> bool:
-        return self.left <= other.left and self.right >= other.right
+    def contains_x(self, other: Box, inclusive: bool = True) -> bool:
+        if inclusive:
+            return self.left <= other.left and self.right >= other.right
+        return self.left < other.left and self.right > other.right
 
-    def contains_y(self, other: Box) -> bool:
-        return self.top <= other.top and self.bottom >= other.bottom
+    def contains_y(self, other: Box, inclusive: bool = True) -> bool:
+        if inclusive:
+            return self.top <= other.top and self.bottom >= other.bottom
+        return self.top < other.top and self.bottom > other.bottom
+
+    def fits_inside(self, other: XY | Box, inclusive: bool = True) -> bool:
+        try:
+            width, height = other.size
+        except AttributeError:
+            width, height = other
+        return self.fits_inside_x(width, inclusive) and self.fits_inside_y(height, inclusive)
+
+    def fits_inside_x(self, width: X, inclusive: bool = True) -> bool:
+        return (width >= self.width) if inclusive else (width > self.width)
+
+    def fits_inside_y(self, height: Y, inclusive: bool = True) -> bool:
+        return (height >= self.height) if inclusive else (height > self.height)
+
+    def fits_around(self, other: XY | Box, inclusive: bool = True) -> bool:
+        try:
+            width, height = other.size
+        except AttributeError:
+            width, height = other
+        return self.fits_around_x(width, inclusive) and self.fits_around_y(height, inclusive)
+
+    def fits_around_x(self, width: X, inclusive: bool = True) -> bool:
+        return (width <= self.width) if inclusive else (width < self.width)
+
+    def fits_around_y(self, height: Y, inclusive: bool = True) -> bool:
+        return (height <= self.height) if inclusive else (height < self.height)
 
     # endregion
 
     # region Size
 
-    @cached_property
+    @cached_property(block=False)
     def width(self) -> int:
         return self.right - self.left
 
-    @cached_property
+    @cached_property(block=False)
     def height(self) -> int:
         return self.bottom - self.top
 
@@ -165,11 +207,11 @@ class Box(Sized):
         """(X, Y) coordinates of the top-left corner of this Rectangle."""
         return self.min_xy
 
-    @cached_property
+    @cached_property(block=False)
     def min_xy(self) -> XY:
         return self.left, self.top
 
-    @cached_property
+    @cached_property(block=False)
     def max_xy(self) -> XY:
         return self.right, self.bottom
 
@@ -184,6 +226,25 @@ class Box(Sized):
     @property
     def bottom_right(self) -> XY:
         return self.max_xy
+
+    # endregion
+
+    # region Resize
+
+    def with_size_offset(self, offset: XY | int, anchor_center: bool = False) -> Box:
+        try:
+            x_off, y_off = offset
+        except TypeError:
+            x_off = y_off = offset
+
+        width, height = self.size
+        x, y = self.min_xy
+        if not anchor_center:
+            return self.from_size_and_pos(width + x_off, height + y_off, x, y)
+
+        dx = (x_off if x_off > 0 else -x_off) // 2
+        dy = (y_off if y_off > 0 else -y_off) // 2
+        return self.from_size_and_pos(width + x_off, height + y_off, x - dx, y - dy)
 
     # endregion
 
