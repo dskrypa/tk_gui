c92dc586d6955899a7bdebb7657da4d74cac8bfd
dskrypa <dskrypa@gmail.com>
2023-08-06 15:30:41 -0400

    refactored file copy popup to use a tree with efficient dst existence checks and fast rejection of existing dst files

--- lib/tk_gui/popups/files.py
+++ lib/tk_gui/popups/files.py
@@ -11,9 +11,8 @@ import os
 from functools import cached_property
 from pathlib import Path
 from shutil import copyfile, copystat
-from stat import S_ISDIR
 from time import monotonic
-from typing import TYPE_CHECKING, Union, Collection, Mapping
+from typing import TYPE_CHECKING, Union, Collection, Mapping, Iterator
 
 from ..elements import Text, ProgressBar
 from ..utils import readable_bytes
@@ -28,6 +27,128 @@ log = logging.getLogger(__name__)
 SrcDstPaths = Union[Collection[tuple[Path, Path]], Mapping[Path, Path]]
 
 
+# region Tree
+
+
+class CopyNode:
+    __slots__ = ('parent', 'root', 'src', 'dst', '_dst_exists')
+
+    def __init__(self, src: Path, dst: Path, parent: CopyDir | None = None):
+        self.parent = parent
+        self.root = parent or self
+        self.src = src
+        self.dst = dst
+        self._dst_exists = None
+
+    def rel_path(self) -> str:
+        if self.parent:
+            if base := self.parent.rel_path():
+                return f'{base}/{self.src.name}'
+            return self.src.name
+        return ''
+
+    def _check_dst_exists(self) -> bool:
+        return self.dst.exists()
+
+    @property
+    def dst_exists(self) -> bool:
+        if self._dst_exists is None:
+            if self.parent and not self.parent.dst_exists:
+                self._dst_exists = False
+            else:
+                self._dst_exists = self._check_dst_exists()
+        return self._dst_exists
+
+    @dst_exists.setter
+    def dst_exists(self, value: bool):
+        self._dst_exists = value
+        if value and self.parent and not self.parent._dst_exists:
+            self.parent.dst_exists = value
+
+
+class CopyDir(CopyNode):
+    __slots__ = ('dirs', 'files')
+
+    def __init__(self, src: Path, dst: Path, parent: CopyDir | None = None):
+        super().__init__(src, dst, parent)
+        self.dirs: list[CopyDir] = []
+        self.files: list[CopyFile] = []
+
+    @classmethod
+    def build_tree(cls, src: Path, dst: Path, parent: CopyDir | None = None) -> CopyDir:
+        node = cls(src, dst, parent)
+        with os.scandir(src) as scanner:
+            for entry in sorted(scanner, key=lambda de: de.name):
+                if entry.is_dir():
+                    node.dirs.append(cls.build_tree(src.joinpath(entry.name), dst.joinpath(entry.name), node))
+                else:
+                    node.files.append(
+                        CopyFile(src.joinpath(entry.name), dst.joinpath(entry.name), entry.stat().st_size, node)
+                    )
+
+        return node
+
+    def _check_dst_exists(self) -> bool:
+        if not self.dst.exists():
+            return False
+        elif not self.dst.is_dir():
+            raise FileExistsError(f'dst_dir={path_repr(self.dst)} already exists, but it is not a directory')
+        return True
+
+    def any_dst_files_exist(self) -> bool:
+        if not self.dst_exists:
+            return False
+        elif any(f.dst_exists for f in self.files):
+            return True
+        return any(d.any_dst_files_exist() for d in self.dirs)
+
+    def any_dst_exists(self, include_self: bool = True) -> bool:
+        if not self.dst_exists:
+            return False
+        elif include_self:
+            return True
+        else:
+            return any(f.dst_exists for f in self.files) or any(d.dst_exists for d in self.dirs)
+
+    def get_existing_files(self) -> list[CopyFile]:
+        return list(self._iter_existing_files())
+
+    def _iter_existing_files(self) -> Iterator[CopyFile]:
+        if not self.dst_exists:
+            return
+        for file_node in self.files:
+            if file_node.dst_exists:
+                yield file_node
+        for dir_node in self.dirs:
+            yield from dir_node._iter_existing_files()
+
+    def iter_files(self) -> Iterator[CopyFile]:
+        yield from self.files
+        for dir_node in self.dirs:
+            yield from dir_node.iter_files()
+
+    def maybe_mkdir_dst(self):
+        if self.dst_exists:
+            return
+        self.dst.mkdir(parents=True, exist_ok=True)
+        self.dst_exists = True
+
+
+class CopyFile(CopyNode):
+    __slots__ = ('size',)
+
+    def __init__(self, src: Path, dst: Path, size: int, parent: CopyDir | None = None):
+        super().__init__(src, dst, parent)
+        self.size = size
+
+    def copy_to_dst(self):
+        copyfile(self.src, self.dst)
+        copystat(self.src, self.dst)
+
+
+# endregion
+
+
 class CopyFilesPopup(Popup):
     progress_bar: ProgressBar
     progress_text: Text
@@ -35,48 +156,27 @@ class CopyFilesPopup(Popup):
     speed_text: Text
     _src_root = None
 
-    def __init__(self, src_dst_paths: SrcDstPaths, src_root: Path = None, **kwargs):
+    def __init__(self, root: CopyDir, **kwargs):
         kwargs['bind_esc'] = False
         kwargs.setdefault('title', 'Copying Files...')
         super().__init__(**kwargs)
-        self.src_root = src_root
-        try:
-            self.src_dst_paths = src_dst_paths.items()
-        except AttributeError:
-            self.src_dst_paths = src_dst_paths
+        self.root = root
 
     @classmethod
     def copy_dir(cls, src_dir: Path, dst_dir: Path, **kwargs) -> CopyFilesPopup:
-        src_dst_paths = []
-        src_dir = src_dir.resolve()
-        for root, dirs, files in os.walk(src_dir):
-            if files:
-                src_root = Path(root)
-                dst_root = dst_dir.joinpath(src_root.relative_to(src_dir))
-                for file in files:
-                    src_dst_paths.append((src_root / file, dst_root / file))
+        node = CopyDir.build_tree(src_dir.resolve(), dst_dir)
+        if existing := node.get_existing_files():
+            msg = f'{len(existing)} path(s) already exist: ' + ', '.join(repr(f.rel_path()) for f in existing)
+            raise FileExistsError(f'Unable to copy src={src_dir.as_posix()} to dst={dst_dir.as_posix()} - {msg}')
+        return cls(node, **kwargs)
 
-        return cls(src_dst_paths, src_dir, **kwargs)
-
-    @property
-    def src_root(self) -> Path:
-        if self._src_root is not None:
-            return self._src_root
-        elif prefix := os.path.commonprefix([src for src, _ in self.src_dst_paths]):
-            prefix = Path(prefix).resolve()
-            if prefix.is_dir():
-                self._src_root = prefix
-                return prefix
-        self._src_root = Path.cwd().resolve()
-        return self._src_root
-
-    @src_root.setter
-    def src_root(self, value: Path):
-        self._src_root = value
+    @cached_property
+    def file_nodes(self) -> list[CopyFile]:
+        return list(self.root.iter_files())
 
     def get_pre_window_layout(self) -> Layout:
-        self.progress_bar = ProgressBar(len(self._src_dst_files), size=(500, 30), side='t')
-        max_path_len = max(len(rel_path) for _, _, rel_path, _ in self._src_dst_files)
+        self.progress_bar = ProgressBar(len(self.file_nodes), size=(500, 30), side='t')
+        max_path_len = max(len(file.rel_path()) for file in self.file_nodes)
         self.progress_text = Text('', size=(min(max_path_len, 180), 1))
         self.file_num_text = Text('', size=(30, 1))
         self.speed_text = Text('', size=(20, 1))
@@ -89,63 +189,19 @@ class CopyFilesPopup(Popup):
         self.copy_files()
         self.window.update()
 
-    @cached_property
-    def _classified_paths(self):
-        files = []
-        to_create = set()
-        exists = set()
-
-        src_root = self.src_root
-        for src_path, dst_path in self.src_dst_paths:
-            src_stat = src_path.stat()
-            if S_ISDIR(src_stat.st_mode):
-                dst_dir = dst_path
-            else:
-                try:
-                    rel_path = src_path.resolve().relative_to(src_root).as_posix()
-                except Exception:  # noqa
-                    rel_path = src_path.name
-                files.append((src_path, dst_path, rel_path, src_stat.st_size))
-                dst_dir = dst_path.parent
-
-            if dst_dir in exists:
-                continue
-            elif dst_dir.is_dir():
-                exists.add(dst_dir)
-            elif dst_dir.exists():
-                raise FileExistsError(f'dst_dir={path_repr(dst_dir)} already exists, but it is not a directory')
-            else:
-                to_create.add(dst_dir)
-        return files, to_create
-
-    @cached_property
-    def _src_dst_files(self) -> list[tuple[Path, Path, str, int]]:
-        return self._classified_paths[0]
-
-    @cached_property
-    def _dirs_to_create(self) -> set[Path]:
-        return self._classified_paths[1]
-
     def copy_files(self):
-        total = len(self._src_dst_files)
+        total = len(self.file_nodes)
         copied = 0
         start = monotonic()
-        for i, (src_path, dst_path, rel_path, size) in enumerate(self._src_dst_files, 1):
+        for i, file_node in enumerate(self.file_nodes, 1):
             self.file_num_text.update(f'{i:,d} / {total:,d}')
-            self._copy_file(src_path, dst_path, rel_path)
-            copied += size
+            file_node.parent.maybe_mkdir_dst()
+            self.progress_text.update(file_node.rel_path())
+            file_node.copy_to_dst()
+            self.progress_bar.increment()
+            copied += file_node.size
             elapsed = (monotonic() - start) or 1
             self.speed_text.update(readable_bytes(copied / elapsed, rate=True))
-
-    def _copy_file(self, src_path: Path, dst_path: Path, rel_path: str):
-        if dst_path.parent in self._dirs_to_create:
-            self._dirs_to_create.remove(dst_path.parent)
-            dst_path.parent.mkdir(parents=True, exist_ok=True)
-
-        self.progress_text.update(rel_path)
-        copyfile(src_path, dst_path)
-        copystat(src_path, dst_path)
-        self.progress_bar.increment()
 
 
 def path_repr(path: Path) -> str:
